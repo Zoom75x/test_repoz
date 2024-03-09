75beef28d2a2579ecaf5e4b1b2ea81561b216e8f
Zoom75x <Zoom-75x@yandex.ru>
2024-03-09 18:52:21 +0900

    Урок Строки

--- jszoom/lesson8/lesson8.js
+++ jszoom/lesson8/lesson8.js
@@ -45,8 +45,26 @@ const getGuestList=()=>{
 //console.log(getLengthStr("\tWorld"))
 
 
+//задача по получению отдельных букв из строки
+
+const getSubString=(str)=>{
+    console.log(str[0]);  //получение первой буквы строки
+    console.log(str.at(0));//получение первой буквы строки
+
+    console.log(str[str.length-1]);   //получение последней буквы строки
+    console.log(str.at(-1))           //получение последней буквы строки
+}
+//console.log(getSubString("Молоко"))
 
 
+
+//получение доступа к каждому элементу строки через функцию и цикл
+
+const getALLElement=(str)=>{
+    for (let i=0; i<str.length-1;i++)
+        console.log(str[i])
+}
+getALLElement("Молоко")
 
 
 
