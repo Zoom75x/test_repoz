fef4695e3b5ba43eb479bbc0ee8dc27d6b04fa04
Zoom75x <Zoom-75x@yandex.ru>
2024-03-10 12:10:49 +0900

    Урок Объекты

---
+++ jszoom/taskObjects/task2/objectBook.js
@@ -0,0 +1,11 @@
+//Задача 2: Доступ к свойствам
+//Описание: Создайте объект book с свойствами title, author, и year.
+// Напишите код, который выводит название книги и год издания в консоль.
+
+let book={
+    title:"Kniga jizni",
+    author:"Ivanov",
+    year:1967
+}
+
+console.log(book.title, book.year)
